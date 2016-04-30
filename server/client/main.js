7cee23544bcd03047492ede845274c8cb2894d78
armando <armandomendivil.m@gmail.com>
2016-04-30 11:03:28 -0700

    List view

--- server/client/main.js
+++ server/client/main.js
@@ -3,20 +3,26 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.home.onCreated(function() {
+  this.subscribe('posts');
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.home.helpers({
+  count() {
+    return Posts.find().count();
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.home.events({
+  'click #increment': function(e) {
+    e.preventDefault();
+
+    Meteor.call('addPost');
   },
-});
+
+  'click #decrement': function(e) {
+    e.preventDefault();
+
+    Meteor.call('deletePost');
+  }
+})
--- server/server/main.js
+++ server/server/main.js
@@ -1,5 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function() {
+  if (Posts.find().count() === 0) {
+    for (i = 1; i <= 10; i++) {
+      Posts.insert({title: 'Post', createdAt: -1});
+    }
+  }
 });
+
+
+Meteor.publish('posts', function() {
+  return Posts.find();
+});
+
