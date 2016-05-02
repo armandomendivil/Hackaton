e2cde03a6afe2f9d38a11bdb6e0a836c8237eca8
armando <armandomendivil.m@gmail.com>
2016-05-01 21:32:06 -0700

    change

--- server/client/main.js
+++ server/client/main.js
@@ -3,37 +3,38 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.home.onCreated(function() {
-  this.subscribe('posts');
-  this.subscribe('employees');
-  this.subscribe('logActivity')
-});
+	// Subscriptions
+	Template.home.onCreated(function() {
+	  this.subscribe('posts');
+	  this.subscribe('employees');
+	  this.subscribe('logActivity')
+	});
+	
+	Template.home.helpers({
+	  count() {
+	    return Posts.find().count();
+	  },
+	  // Log from activity
+	  log() {
+	  		return  LogActivity.find({}, {sort: {createdAt: -1}})
+	  	}
+		});
+		// Add Events
+		Template.home.events({
+		  'click #increment': function(e) {
+		    e.preventDefault();
 
-Template.home.helpers({
-  count() {
-    return Posts.find().count();
-  },
-
-  log() {
-  	return  LogActivity.find({}, {sort: {createdAt: -1}})
-  }
-});
-
-Template.home.events({
-  'click #increment': function(e) {
-    e.preventDefault();
-
-    Meteor.call('addPost');
-  },
-
-  'click #addActivity': function(e) {
-    e.preventDefault();
-    debugger
-    var postProperties = {
-      title: $('#activity').val(),
-      createdAt: new Date()
-    }
-    Meteor.call('editTask', postProperties);
-  }
-});
+		    Meteor.call('addPost');
+		  },
+		  // Add activity
+	  'click #addActivity': function(e) {
+	    e.preventDefault();
+	    debugger
+	    var postProperties = {
+	      title: $('#activity').val(),
+	      createdAt: new Date()
+	    }
+	    Meteor.call('editTask', postProperties);
+	  }
+	});
 
